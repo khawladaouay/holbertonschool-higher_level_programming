bf20173d08afeebabf45026a7f6d19497e8b1928
khawladaouay <daouaykhawla782@gmail.com>
2022-04-29 12:35:26 +0100

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(n - 1))
         for i in range(1, n):
-           print("{}: {}".format(i, argv[i]))
+            print("{}: {}".format(i, argv[i]))
