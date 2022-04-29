2922be1e9d7a7128a4493f5cd470c8d106488466
khawladaouay <daouaykhawla782@gmail.com>
2022-04-29 20:35:42 +0100

    task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    argc = len(argv)
+    i = 1
+    sum = 0
+    if argc == 1:
+        print("0")
+    else:
+        while i < argc:
+            sum += int(argv[i])
+            i += 1
+    print(sum)
