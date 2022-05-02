770282799d08cca7f319f383050ec3ccb58179b2
khawladaouay <daouaykhawla782@gmail.com>
2022-05-02 23:10:19 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        my_list.sort()
+        for i in my_list:
+            continue
+        return i
+    else:
+        return None
