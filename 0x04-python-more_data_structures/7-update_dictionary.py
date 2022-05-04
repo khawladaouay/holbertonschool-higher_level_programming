848c8bd3aabce07a976c5ea332c065c8827ee395
khawladaouay <daouaykhawla782@gmail.com>
2022-05-04 16:34:39 +0100

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from re import A
+
+
+def update_dictionary(a_dictionary, key, value):
+    for i in a_dictionary.keys():
+        if key == i:
+            a_dictionary.update({i: value})
+    a_dictionary["key"] = value
+    return a_dictionary
