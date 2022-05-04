5eb7479cf9a18c63f0aaf5d38b243ae31d696ef5
khawladaouay <daouaykhawla782@gmail.com>
2022-05-04 16:47:39 +0100

    task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,8 @@ from re import A
 
 
 def update_dictionary(a_dictionary, key, value):
-    for i in a_dictionary.keys():
-        if key == i:
-            a_dictionary.update({i: value})
-    a_dictionary[key] = value
+    if key in a_dictionary.keys():
+        a_dictionary.update({key: value})
+    else:
+        a_dictionary[key] = value
     return a_dictionary
