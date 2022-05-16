9774fbb78e20e9b042b5ec7a985b97ac6107b73c
khawladaouay <daouaykhawla782@gmail.com>
2022-05-16 20:12:34 +0100

    task 3

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        resul = a / b
+        return resul
+    except ZeroDivisionError:
+        resul = None
+        return resul
+    finally:
+        print("Insid result: {}".format(resul))
