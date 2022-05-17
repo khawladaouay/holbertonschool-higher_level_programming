b403dd6e9d890495fb42710bc0b59929e8240fae
khawladaouay <daouaykhawla782@gmail.com>
2022-05-17 16:05:01 +0100

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""" this is a square"""
+
+
+class Square:
+    """Square Class
+This square has a size
+"""
+    def __init__(self, size):
+        """__init__
+        initializing square
+        """
+        self.__size = size
