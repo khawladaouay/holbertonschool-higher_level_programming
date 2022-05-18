50c5237c9892c21652f9b3238203f452fb38ad4c
khawladaouay <daouaykhawla782@gmail.com>
2022-05-18 22:36:29 +0100

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,45 +1,49 @@
 #!/usr/bin/python3
-"""" this is a square"""
+"""task 6"""
 
 
 class Square:
-    """Square Class"""
+    """class that defines a square"""
+
     def __init__(self, size=0, position=(0, 0)):
-        """__init__
-        initializing square
-        """
+        """inisialising"""
         self.__size = size
         self.position = position
 
     @property
     def size(self):
-        """ value of size"""
+        """do size"""
         return self.__size
-
-    @size.setter
-    def size(self, value):
-        """ set the value of size"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
-
-    def area(self):
-        """square area"""
-        return self.__size*self.__size
 
     @property
     def position(self):
+        """getting position"""
         return self.__position
 
     @size.setter
+    def size(self, value):
+        """set size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            """return the square"""
+            self.__size = value
+
+    @position.setter
     def position(self, value):
-        self.__position = value
+        """set position"""
         if type(value) is not tuple or len(value) != 2 or \
            type(value[0]) is not int or value[0] < 0 or \
            type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        """getting size square"""
+        return self.__size ** 2
 
     def my_print(self):
         """printing a square"""
