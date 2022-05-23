fb298b4cca6bb3b055c817f5b5a948a6003b879f
khawladaouay <daouaykhawla782@gmail.com>
2022-05-23 15:26:15 +0100

    task 3

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""""this is a Rectangle"""
+
+
+class Rectangle:
+    """"Rectangle class"""
+    def __init__(self, width=0, height=0):
+        """inisialising"""
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+        if type(value) != int:
+            TypeError("width must be an integer")
+        if value < 0:
+            ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+        if type(value) != int:
+            TypeError("height must be an integer")
+        if value < 0:
+            ValueError("height must be >= 0")
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__height + self.__width)*2
+
+    def print(self):
+        if self.__width == 0 or self.__height == 0:
+            return print("")
+        else:
+            for i in range(self.height):
+                for i in range(self.width):
+                    print("#", end="")
+
+    def __str__(self):
+        if self.__width == 0 or self.height == 0:
+            return ""
+        mat = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                mat += "#"
+            if j != self.__width:
+                mat += '\n'
+        return mat
