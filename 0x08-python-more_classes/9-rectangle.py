a23003310cf5ec2e16842c6fec539761ec1481a5
khawladaouay <daouaykhawla782@gmail.com>
2022-05-24 11:21:17 +0100

    task 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -19,11 +19,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        self.__width = value
         if type(value) != int:
             TypeError("width must be an integer")
         if value < 0:
             ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -31,11 +31,11 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        self.__height = value
         if type(value) != int:
             TypeError("height must be an integer")
         if value < 0:
             ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
         return self.__height * self.__width
