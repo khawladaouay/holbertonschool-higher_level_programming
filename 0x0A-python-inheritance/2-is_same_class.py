f17a287e713d2256df7920ebf91234c80fa1db0f
khawladaouay <daouaykhawla782@gmail.com>
2022-05-30 18:32:02 +0100

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""""is_same_class function
+this function checks if obj is same class as a_class
+"""
+
+
+def is_same_class(obj, a_class):
+    """is_same_class function
+    this function checks if obj is same class as a_class
+    """
+
+    return type(obj) is a_class
