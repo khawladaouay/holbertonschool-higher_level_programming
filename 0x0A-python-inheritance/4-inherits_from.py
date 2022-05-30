7a94e547a69bb5c5aacd88de41478a4aa5ac69fd
khawladaouay <daouaykhawla782@gmail.com>
2022-05-30 18:37:57 +0100

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,8 @@ Checks if object's class inherits from another class
 
 
 def inherits_from(obj, a_class):
-	"""inherits_from function
+    """inherits_from function
     this function checks for inheritance
     """
 
-	return type(obj) !=  a_class
+    return issubclass(obj.__class__, a_class) and type(obj) != a_class
