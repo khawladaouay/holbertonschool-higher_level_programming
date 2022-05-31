081794c1ef9f9885852a5483ffebbeedefddf97d
khawladaouay <daouaykhawla782@gmail.com>
2022-05-31 16:57:34 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""""read_file module"""
+
+
 def read_file(filename=""):
+    """"reaf_file function
+    this function reads file with 'filename'
+    """
+
     with open(filename, encoding="utf-8") as f:
         print(f.read())
