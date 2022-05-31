cc277d0062317ca01a0dad21f8987278664de31a
khawladaouay <daouaykhawla782@gmail.com>
2022-05-31 17:11:36 +0100

    task 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""from_json_string"""
 import json
 
 
 def from_json_string(my_str):
+    """from_json_string unction that returns an object (Python data structure)
+    represented by a JSON string
+    """
     return json.loads(my_str)
