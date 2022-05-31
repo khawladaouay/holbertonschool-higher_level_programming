3fbfe256c3077601178094b44673aa81ce95b41b
khawladaouay <daouaykhawla782@gmail.com>
2022-05-31 17:06:55 +0100

    task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+""""append_write"""
+
+
 def append_write(filename="", text=""):
+    """"append_write function that appends a string at
+    the end of a test file
+    """
+
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
