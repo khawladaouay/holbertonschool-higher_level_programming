d98928904dcf65d5d90e928df9289f2a222373c8
khawladaouay <daouaykhawla782@gmail.com>
2022-05-31 17:01:49 +0100

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""""write_file module"""
+
+
 def write_file(filename="", text=""):
-	with open(filename, 'w', encoding="utf-8") as f:
-		return f.write(text)
+    """"write_file function that writes a string to  text file"""
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
