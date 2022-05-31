4027bc159547fb8877fe0a53741b31d23495a065
khawladaouay <daouaykhawla782@gmail.com>
2022-05-31 20:38:35 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""class"""
+
+
+class Student:
+    """Student class defines a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        my_dict = dict()
+
+        if type(attrs) is list:
+            for elem in attrs:
+                if type(elem) is not str:
+                    return self.__dict__
+
+                if elem in self.__dict__:
+                    my_dict[elem] = self.__dict__[elem]
+
+            return my_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """reload_from_json
+        replaces all attributes of the Student instance
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
