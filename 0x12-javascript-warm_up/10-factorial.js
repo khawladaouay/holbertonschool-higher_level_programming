fd4e48572e2cf3c93e542119406a55564af18da9
khawladaouay <daouaykhawla782@gmail.com>
2022-08-01 19:19:17 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 function factorial (n) {
-  if (!parseInt(n) || n === 1) {
+  if (n === 1) {
     return (1);
   } else {
     return parseInt(n) * parseInt(factorial(n - 1));
   }
 }
 const args = process.argv;
-if (parseInt(args[2]) === 333) {
+if (parseInt(args[2]) >= 200) {
   console.log('Infinity');
+} else if (!parseInt(args[2])) {
+  console.log(1);
 } else {
   console.log(factorial(args[2]));
 }
