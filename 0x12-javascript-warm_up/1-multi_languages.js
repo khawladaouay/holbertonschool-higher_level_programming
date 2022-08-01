96ee1b97ce2b32c6793af1c06718ea3f9ffe37b8
khawladaouay <daouaykhawla782@gmail.com>
2022-08-01 12:13:43 +0100

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-console.log('C is fun \nPython is cool \nJavaScript is amazing');
+const a = 'C is fun';
+const b = 'Python is cool';
+const c = 'JavaScript is amazing';
+console.log(a);
+console.log(b);
+console.log(c);
