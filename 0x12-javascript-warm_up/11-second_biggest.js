6e585204508a28efd1601b3cd3354b17e6f3bdca
khawladaouay <daouaykhawla782@gmail.com>
2022-08-01 19:44:06 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
-let bignum = args[2];
+let bignum = parseInt(args[2]);
 if (args.length <= 3) {
   console.log(0);
 } else {
   for (let i = 0; i < args.length; i++) {
-    if (bignum < args[i]) {
+    if (bignum < parseInt(args[i])) {
       bignum = args[i];
     }
   }
