a6ba7bfd25649c46fa827e15122d970f4d32a38d
khawladaouay <daouaykhawla782@gmail.com>
2022-08-01 12:23:09 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 const args = process.argv;
 if (args.length < 3) {
   console.log('No argument');
+} else if (args.length === 3) {
+  console.log('Argument found');
 } else {
   console.log('Argument found');
 }
