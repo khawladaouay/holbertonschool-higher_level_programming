bc4b685da834d36e1a35193bdb04442acee0d62d
khawladaouay <daouaykhawla782@gmail.com>
2022-08-02 21:20:23 +0100

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,10 +9,9 @@ module.exports = class Rectangle {
 
   print () {
     let string = '';
-    for (let i = 0; i < this.width; i++)
-      string += 'X';
-      for (let i = 0; i < this.height; i++) {
-        console.log(string);
-      }
+    for (let i = 0; i < this.width; i++) { string += 'X'; }
+    for (let i = 0; i < this.height; i++) {
+      console.log(string);
+    }
   }
 };
