4a837c6cf01b7e653919a536c733a7d4c667e1c0
khawladaouay <daouaykhawla782@gmail.com>
2022-08-07 10:11:57 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in the states where name matches the arg"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    """make a connectin"""
+    db_connection = MySQLdb.connect(host='localhost',
+                                    port=3306,
+                                    user=argv[1],
+                                    password=argv[2],
+                                    database=argv[3])
+
+    """create a cursor"""
+    c = db_connection.cursor()
+
+    """execute query"""
+    c.execute("SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC".format(argv[4]))
+    n = c.fetchall()
+    for i in n:
+        print(i)
+    
+
+    """close all cursor and database"""
+    c.close()
+    db_connection.close()
