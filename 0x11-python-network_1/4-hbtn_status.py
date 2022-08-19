2cdc8f81c52ef45a6a4f8bba659a53e80251685a
khawladaouay <daouaykhawla782@gmail.com>
2022-08-19 14:26:16 +0100

    task 4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""fetches  https://intranet.hbtn.io/status"""
+import requests
+if __name__ == '__main__':
+    date = requests.get(' https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(date.text)))
+    print("\t- content: {}".format(date.text))
