1ba63131c9bb067db9ab46561f5e7c076f3f27c4
khawladaouay <daouaykhawla782@gmail.com>
2022-08-19 14:32:32 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """fetches  https://intranet.hbtn.io/status"""
 import requests
-if __name__ == '__main__':
-    date = requests.get(' https://intranet.hbtn.io/status')
+
+if __name__ == "__main__":
+    date = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(date.text)))
     print("\t- content: {}".format(date.text))
