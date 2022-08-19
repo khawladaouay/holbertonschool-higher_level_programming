d998508774fc5c4eb1d8b71497b7732edcd10b1f
khawladaouay <daouaykhawla782@gmail.com>
2022-08-19 13:21:56 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """
 script that fetches url"""
+import urllib.request
+
 
 if __name__ == '__main__':
-    import urllib.request
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as f:
         data = f.read()
         print("Body response:")
-        print("- type: {}".format(type(data)))
-        print("- content: {}".format(data))
-        print("- utf8 content: {}".format(data.decode('utf8')))
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode('utf8')))
