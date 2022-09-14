591404f08378645e34a17d565859d52027b2f2b7
khawladaouay <daouaykhawla782@gmail.com>
2022-09-14 11:29:22 +0100

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const file = require('fs');
-file.writeFile(process.argv[2], 'utf8', function (error, content) {
+file.writeFile(process.argv[2], 'utf8', read);
+function read (error, content) {
   console.log(error || content);
-});
+}
