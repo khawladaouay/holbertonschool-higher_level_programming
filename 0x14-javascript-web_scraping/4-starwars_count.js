09804e446bbab11894296aa35193410c6841683c
khawladaouay <daouaykhawla782@gmail.com>
2022-09-14 14:16:54 +0100

    task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+const axios = require('axios').default;
+const reqUrl = process.argv[2];
+let count = 0;
+axios.get(reqUrl)
+  .then(function (response) {
+    for (let i = 0; i < response.data.results.length; i++) {
+      const arr = response.data.results[i].characters;
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j].includes('18')) {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
