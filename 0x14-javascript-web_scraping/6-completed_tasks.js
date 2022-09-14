caf0e18a964ead11a2cc6fea8c3b8b56fa600a30
khawladaouay <daouaykhawla782@gmail.com>
2022-09-14 15:14:37 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,15 @@
 #!/usr/bin/node
-const axios = require('axios').default;
-const Url = process.argv[2];
-const dic = {};
-let i = 0;
-axios.get(Url)
-  .then(function (response) {
-    for (i = 0; i < response.data.length; i++) {
-      if (!dic[response.data[i].userId]) {
-        dic[response.data[i].userId] = 0;
-      }
-      if (response.data[i].completed === true) {
-        dic[response.data[i].userId] += 1;
+const request = require('request');
+request(process.argv[2], function (err, f, body) {
+  const dic = {};
+  for (const res of JSON.parse(body)) {
+    if (res.completed) {
+      if (dic[res.userId]) {
+        data[res.userId] += 1;
+      } else {
+        dic[res.userId] = 1;
       }
     }
-    console.log(dic);
-  });
+  }
+  console.log(data);
+});
